c5ef9c67f1c1fb919e0c4d5b411b1df2c0b8e2e8
Isaiah042 <isaiahgarcia0425@gmail.com>
2024-06-13 12:58:40 -0400

    El-Dorado-Bank

---
+++ src/main/java/com/eldorado/El_Dorado/ElDoradoApplication.java
@@ -0,0 +1,13 @@
+package com.eldorado.El_Dorado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElDoradoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElDoradoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eldorado/El_Dorado/ElDoradoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eldorado.El_Dorado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElDoradoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
