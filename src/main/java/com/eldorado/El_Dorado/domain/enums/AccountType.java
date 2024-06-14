9e09e5ced5f6e21e33dac4e9be3821a6015a061d
JuanL <laravega.juan8@gmail.com>
2024-06-13 21:59:16 -0400

    Account enum and class

--- src/main/java/com/eldorado/El_Dorado/domain/enums/AccountType.java
+++ src/main/java/com/eldorado/El_Dorado/domain/enums/AccountType.java
@@ -1,6 +1,7 @@
 package com.eldorado.El_Dorado.domain.enums;
 
 public enum AccountType {
+    SAVINGS, CHECKING, CREDIT
 }
 //An enumerated value representing the Status of the bill that serializes to a
 //string
