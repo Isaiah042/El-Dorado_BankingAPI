98f7e007e3fd9d8572f886c9f2d517f7c9ebb66e
Ashyasp <ashyas427@gmail.com>
2024-06-13 22:26:06 -0400

    Bill class

--- src/main/java/com/eldorado/El_Dorado/domain/Bill.java
+++ src/main/java/com/eldorado/El_Dorado/domain/Bill.java
@@ -1,10 +1,105 @@
 package com.eldorado.El_Dorado.domain;
 
 
+import jakarta.persistence.GeneratedValue;
+import org.springframework.data.annotation.Id;
+import java.time.LocalDate;
 
 
 public class Bill {
-
     //Variables
+@Id
+@GeneratedValue
+private Long id;
+    private String status;
 
+    private String payee;
+    private String nickname;
+    private LocalDate creationDate;
+    private LocalDate paymentDate;
+    private Integer recurringDate;
+    private LocalDate upcomingPaymentDate;
+    private Double paymentAmount;
+    private Long accountId;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPayee() {
+        return payee;
+    }
+
+    public void setPayee(String payee) {
+        this.payee = payee;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public LocalDate getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDate creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDate getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(LocalDate paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public Integer getRecurringDate() {
+        return recurringDate;
+    }
+
+    public void setRecurringDate(Integer recurringDate) {
+        this.recurringDate = recurringDate;
+    }
+
+    public LocalDate getUpcomingPaymentDate() {
+        return upcomingPaymentDate;
+    }
+
+    public void setUpcomingPaymentDate(LocalDate upcomingPaymentDate) {
+        this.upcomingPaymentDate = upcomingPaymentDate;
+    }
+
+    public Double getPaymentAmount() {
+        return paymentAmount;
+    }
+
+    public void setPaymentAmount(Double paymentAmount) {
+        this.paymentAmount = paymentAmount;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
 }
