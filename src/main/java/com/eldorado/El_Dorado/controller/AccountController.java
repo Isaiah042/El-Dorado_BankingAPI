f3f8cf8eee60465afb2992b9c0fbe1628857e63d
Isaiah042 <isaiahgarcia0425@gmail.com>
2024-06-17 09:34:16 -0400

    New Work

--- src/main/java/com/eldorado/El_Dorado/controller/AccountController.java
+++ src/main/java/com/eldorado/El_Dorado/controller/AccountController.java
@@ -1,12 +1,10 @@
 package com.eldorado.El_Dorado.controller;
 
-<<<<<<< HEAD
 import com.eldorado.El_Dorado.service.AccountService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-=======
 import com.eldorado.El_Dorado.domain.Account;
 import com.eldorado.El_Dorado.repository.AccountRepo;
 import com.eldorado.El_Dorado.repository.CustomerRepo;
