304b428da670481cc310ab2d0cd58970c5a2701a
TreyComic <trey.greene1153@gmail.com>
2024-06-18 11:53:55 -0400

    changes to service and controller of withdrawal

--- src/main/java/com/eldorado/El_Dorado/controller/WithdrawalController.java
+++ src/main/java/com/eldorado/El_Dorado/controller/WithdrawalController.java
@@ -1,36 +1,50 @@
 package com.eldorado.El_Dorado.controller;
 
+import com.eldorado.El_Dorado.domain.Withdrawal;
 import com.eldorado.El_Dorado.repository.WithdrawalRepository;
+import com.eldorado.El_Dorado.service.WithdrawalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import java.util.Optional;
+
 
 @RestController
-public class WithdrawalController {
+public class WithdrawalController extends WithdrawalService {
 
     @Autowired
-    private WithdrawalRepository withdrawalRepository;
+    private WithdrawalService withdrawalService;
     @GetMapping("accounts/{accountId}/withdrawals")
-    public ResponseEntity<?> getAllAccountWithdrawals(@PathVariable Long accountId){
-        return null;
+    public Optional<Withdrawal> getAllAccountWithdrawalsByAccountId(@PathVariable Long accountId){
+        return withdrawalService.getWithdrawalById(accountId);
     }
 
     @GetMapping("/withdrawals/{withdrawalId}")
-    public ResponseEntity<?> getWithdrawalById(@PathVariable Long withdrawalId){
-        return null;
+    public Optional<Withdrawal> getWithdrawalById(@PathVariable Long withdrawalId){
+        return withdrawalService.getWithdrawalById(withdrawalId);
     }
 
     @PostMapping("accounts/{accountId}/withdrawals")
-    public void makeNewWithdrawal(@PathVariable Long accountId){
-    }
-
-    @PutMapping("withdrawals/{withdrawalId}")
-    public ResponseEntity<?> updateExistingWithdrawals(@PathVariable Long withdrawalId){
+    public ResponseEntity<?> makeNewWithdrawal(Withdrawal withdrawal){
+        withdrawalService.saveWithdrawal(withdrawal);
         return null;
     }
 
+    @PutMapping("withdrawals/{withdrawalId}")
+    public ResponseEntity<?> updateExistingWithdrawals(@PathVariable Long withdrawalId, @RequestBody Withdrawal withdrawal){
+        withdrawalService.updateWithdrawal(withdrawalId, withdrawal);
+        return null;
+    }
+
+    //ResponseEntity<?>
     @DeleteMapping("withdrawals/{withdrawalId}")
     public ResponseEntity<?> deleteExistingWithdrawal (@PathVariable Long withdrawalId){
+        withdrawalService.deleteWithdrawalById(withdrawalId);
         return null;
     }
 
--- src/main/java/com/eldorado/El_Dorado/service/WithdrawalService.java
+++ src/main/java/com/eldorado/El_Dorado/service/WithdrawalService.java
@@ -1,8 +1,11 @@
 package com.eldorado.El_Dorado.service;
 import com.eldorado.El_Dorado.domain.Withdrawal;
+import com.eldorado.El_Dorado.exception.ResourceNotFoundException;
+import com.eldorado.El_Dorado.repository.DepositRepository;
 import com.eldorado.El_Dorado.repository.WithdrawalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,33 +15,24 @@ public class WithdrawalService {
     @Autowired
     private WithdrawalRepository withdrawalRepository;
 
-    //timestamp of current datetime as string on creation of a deposit
-    /**
-     * Get withdrawals for a specific account
-     *
-     * Get withdrawal by id
-     *
-     * POST Create a withdrawal
-     *
-     * PUT Update a specified existing withdrawal
-     *
-     * DELETE Delete a specific existing withdrawal
-     */
+    @Autowired
+    private DepositRepository depositRepository;
 
-
-    public Withdrawal saveWithdrawal(Withdrawal withdrawal) {
-        return withdrawalRepository.save(withdrawal);
+    public void saveWithdrawal(Withdrawal withdrawal) {
+        withdrawalRepository.save(withdrawal);
     }
 
-    public List<Withdrawal> getAllWithdrawals() {
+    public Iterable<Withdrawal> getAllWithdrawals() {
         return (List<Withdrawal>) withdrawalRepository.findAll();
     }
 
     public Optional<Withdrawal> getWithdrawalById(Long id) {
+        verifyWithdrawal(id);
         return withdrawalRepository.findById(id);
     }
 
-    public Withdrawal updateWithdrawal(Long id, Withdrawal withdrawal) {
+    public void updateWithdrawal(Long id, Withdrawal withdrawal) {
+        verifyWithdrawal(id);
         Optional<Withdrawal> existingWithdrawal = withdrawalRepository.findById(id);
         if (existingWithdrawal.isPresent()) {
             Withdrawal updatedWithdrawal = existingWithdrawal.get();
@@ -47,13 +41,22 @@ public class WithdrawalService {
             updatedWithdrawal.setMedium(withdrawal.getMedium());
             updatedWithdrawal.setAmount(withdrawal.getAmount());
             updatedWithdrawal.setDescription(withdrawal.getDescription());
-            return withdrawalRepository.save(updatedWithdrawal);
+            withdrawalRepository.save(updatedWithdrawal);
         }
-        return null;
+
     }
 
-    public void deleteWithdrawal(Long id) {
+    public void deleteWithdrawalById(Long id) {
+        verifyWithdrawal(id);
+
         withdrawalRepository.deleteById(id);
+    }
+
+    public void verifyWithdrawal(Long withdrawalId) throws ResourceNotFoundException {
+        Withdrawal w = withdrawalRepository.findById(withdrawalId).orElse(null);
+        if (w == null){
+            throw new ResourceNotFoundException("Withdrawal with id " + withdrawalId + " not found");
+        }
     }
 
 }
