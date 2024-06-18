6d95613dfe4aeae668c458812dbeeb61edde9b16
elcorkum <lizzycorkum@gmail.com>
2024-06-18 14:47:15 -0400

    Debugs

--- src/main/java/com/eldorado/El_Dorado/domain/Customer.java
+++ src/main/java/com/eldorado/El_Dorado/domain/Customer.java
@@ -11,7 +11,7 @@ import java.util.Set;
 public class Customer {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotEmpty
@@ -23,24 +23,23 @@ public class Customer {
     private String lastName;
 
 
-    @OneToMany
-    @JoinColumn(name= "CUSTOMER_ID")
-    private Set<Address> addresses;
+//    @OneToMany(cascade = CascadeType.ALL)
+//    @JoinColumn(name= "CUSTOMER_ID")
+//    private Set<Address> addresses;
 
 
-    @OneToMany(mappedBy = "customer")
-    @JoinColumn(name= "CUSTOMER_ID" )
-    private Set<Account> accounts;
+//    @OneToMany(mappedBy = "customer")
+//    private Set<Account> accounts;
 
     public Customer() {
     }
 
-    public Customer(Long id, String firstName, String lastName, Set<Address> addresses, Set<Account> accounts) {
+    public Customer(Long id, String firstName, String lastName) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.addresses = addresses;
-        this.accounts = accounts;
+//        this.addresses = addresses;
+//        this.accounts = accounts;
     }
 
     public Long getId() {
@@ -67,19 +66,19 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public Set<Address> getAddresses() {
-        return addresses;
-    }
-
-    public void setAddresses(Set<Address> addresses) {
-        this.addresses = addresses;
-    }
-
-    public Set<Account> getAccounts() {
-        return accounts;
-    }
-
-    public void setAccounts(Set<Account> accounts) {
-        this.accounts = accounts;
-    }
+//    public Set<Address> getAddresses() {
+//        return addresses;
+//    }
+//
+//    public void setAddresses(Set<Address> addresses) {
+//        this.addresses = addresses;
+//    }
+//
+//    public Set<Account> getAccounts() {
+//        return accounts;
+//    }
+//
+//    public void setAccounts(Set<Account> accounts) {
+//        this.accounts = accounts;
+//    }
 }
