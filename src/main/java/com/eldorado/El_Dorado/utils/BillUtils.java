cd64b2db36546c0d3d6928d6e30e9d7d80928c4e
Isaiah042 <isaiahgarcia0425@gmail.com>
2024-06-24 15:53:26 -0400

    BillUtil

--- src/main/java/com/eldorado/El_Dorado/utils/BillUtils.java
+++ src/main/java/com/eldorado/El_Dorado/utils/BillUtils.java
@@ -1,4 +1,21 @@
 package com.eldorado.El_Dorado.utils;
 
+import com.eldorado.El_Dorado.domain.Account;
+
 public class BillUtils {
+
+    public static void takingMoneyFromAccountToPayBIll(Account account, Double bill){
+        double currentBalance = account.getBalance();
+        double newBalance = currentBalance - bill;
+        account.setBalance(newBalance);
+    }
+
+    public static void takingFromRewardsToPayBill(Account account, Double rewards){
+        double currentRewards = account.getRewards();
+        double newRewards = currentRewards - rewards;
+        account.setRewards((int) newRewards);
+    }
+
+
+
 }
